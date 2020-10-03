ac63bcd8aabeb3c1ac03748ee90216147354c1d9
Natalia Andrade <naty17and@gmail.com>
2020-10-03 00:52:05 -0300

    subindo arquivos para o git commit

---
+++ script.js
@@ -0,0 +1,20 @@
+/*let items=document.querySelectorAll('ul > li');
+
+items.forEach(item => {
+    // ação
+    item.onmouseover =function() {
+        document.querySelector('ul > li').classList.remove('active');
+        item.classList.add('active')
+              
+    }
+    
+})
+items.forEach(item => {
+    // ação
+    /*item.onmouseleave =function() {
+        document.querySelector('ul > li').classList.remove('active');
+        /*item.classList.add('socialMedia')*/
+        
+    /*}*/
+    
+/*})*/
